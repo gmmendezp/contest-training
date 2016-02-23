585f4e696ae9ff6785ce441d02001bb88f13640e
Martin Mendez <gmmendezp@gmail.com>
2016-02-22 20:39:31 -0600

    Improved python baseline.

--- baselines/main.py
+++ baselines/main.py
@@ -1,21 +1,26 @@
+import sys
+
 def solve():
-    T = int(interface.r()) + 1
-    f = "Case #%d: %s";
+    T = int(io.readline()) + 1
+    f = "Case #{0}: {1}";
     for t in range(1,T):
-        interface.w(f % (t, interface.r()))
+        io.write(f.format(t, io.readline()))
 
-class interface:
-    @staticmethod
-    def w(*s):
-        fout.write(s + "\n")
-        #print(*s)
-    @staticmethod
-    def r():
-        return fin.readline().rstrip()
-        #return input()
+class InOut:
+    def __init__(self, inFile=True, outFile=True):
+        self.fin = open('test.in', 'r') if inFile else sys.stdin
+        self.fout = open('test.out', 'w') if outFile else sys.stdout
+    def __enter__(self):
+        return self
+    def __exit__(self, type, value, traceback):
+        self.fin.close()
+        self.fout.close()
+    def write(self, *s, sep='', end='\n'):
+        print(*s, sep=sep, end=end, file=self.fout)
+    def readline(self):
+        return self.fin.readline().rstrip()
+    def read(self):
+        return self.fin.read().rstrip()
 
-fin = open('test.in', 'r')
-fout = open('test.out', 'w')
-solve()
-fin.close()
-fout.close()
+with InOut(False, False) as io:
+    solve()
